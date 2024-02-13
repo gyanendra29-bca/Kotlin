e20c47e1e9678ee6c137bc72ecaa855f0bdac092
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-13 15:48:07 +0530

    second commit

---
+++ variables.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var rollNum =10 // var type variable we can reasign
+    var mobileNumber = 1234567890L
+    var price= 55.9f
+    var gender = 'M'
+    var name = "gyanendra tiwari"
+    println("rollNum="+ rollNum)
+    println("mobileNumber="+ mobileNumber)
+    println("price="+ price)
+    println("gender="+ gender)
+    println("name="+ name)
+    println("*******************")
+
+    val rollnum:Int =100 // val type variable cant reasign
+    var Price= 55.9f
+    val Gender = 'M'
+    val Name = "gyanendra tiwari"
+    println("rollnum="+ rollNum)
+    println("MobileNumber="+ mobileNumber)
+    println("Price="+ price)
+    println("Gender="+ gender)
+    println("Name="+ name)
+
+
+
+}
