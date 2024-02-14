7eecbc122d79458c1962f64fbcb154ba758c44e9
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-14 13:29:15 +0530

    4th commit

---
+++ arthematic opertions.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a=10
+    val b=20
+    println("addition=${a+b}")
+    println("subtraction=${a-b}")
+    println("multipication=${a*b}")
+    println("division=${a/b}")
+    println("modulas=${a%b}")
+}
