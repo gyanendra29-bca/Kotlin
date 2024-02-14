faeef39360b892f836a030283602108ce8e648d9
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-14 13:22:37 +0530

    3rd commit

---
+++ printFunction.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a = 10
+    val b = 20
+    print(a)
+    print(b)
+    println(a)
+    println(b)
+    println("***************")
+    println("a="+a)
+    println("a=$a")
+    println("b="+b)
+    println("b=$b")
+    println("a=$a and b=$b")
+
+}
