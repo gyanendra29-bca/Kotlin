24ebeda317b1acc4f7931a25843115304fcfb1ff
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-15 12:23:14 +0530

    9th commit

---
+++ Agumented assignment opertaors.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var a=10
+
+    a+=5
+    println("addition agumentrd = $a")
+    a-=5
+    println("subtraction agumented = $a")
+    a*=5
+    println("multiplication agumented = $a")
+    a/=5
+    println("division agumented = $a")
+    a%=5
+    println("modulas agumented = $a")
+
+
+
+
+
+
+}
