305234b1a8fb2c4b77a03d6c7fd96ddca95139b3
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-15 12:10:04 +0530

    8th commit

---
+++ increament and decreament operator.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a=10
+    println(a)
+    println("*******increament*****")
+    println(++a) // this is prefix increament
+    println(a++) // this is postfix increament
+    println(a)
+    println("********decreament******")
+    println(--a) // this is prefix decreament
+    println(a--) // this is postfix decreament
+    println(a)
+
+}
