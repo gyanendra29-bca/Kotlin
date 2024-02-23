906ff3fb5abe03ecec9fe58fe5b931899282db4e
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-23 14:44:01 +0530

    11th commit

---
+++ stringConcat.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    println("enter str1")
+    val str1 =sc.nextLine()
+    println("enetr str2")
+    val str2 =sc.nextLine()
+    println(str1 + str2)
+}
