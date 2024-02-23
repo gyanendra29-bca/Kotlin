d8ac78d527fe214d2eed4750ef09bff773a9288f
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-23 14:56:39 +0530

    13th commit

--- StringRaw.kt
+++ StringRaw.kt
@@ -2,6 +2,6 @@ fun main() {
     val message="""        Intro 
         my name is gyanendra kumar tiwari
         i am curently learning kotlin 
-    """
+    """.trimIndent()
     println(message)
 }
