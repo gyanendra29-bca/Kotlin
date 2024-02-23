c100cfeafa1ae1145612968ff4004d33196b15c2
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-23 15:29:17 +0530

    17th commit

---
+++ when expression.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val x=1
+    when(x) // when expression is same as if else if lader
+    {
+        1->
+            println("good moring")
+        2 ->
+            println("good afternoon")
+        3 ->
+            println("good evening")
+        4 ->
+            println("good night")
+
+    }
+
+
+}
