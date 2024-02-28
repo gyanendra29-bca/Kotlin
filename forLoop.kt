d21dd613841bcbf578ceacdeae8bddb92fdc03d4
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-28 21:21:15 +0530

    18th commit

---
+++ forLoop.kt
@@ -0,0 +1,13 @@
+fun main() {
+    for(item in 1..5) println(item)
+
+    for(item in 1..5) {
+       print("Step: ")
+        println(item)
+   }
+
+    for(item in 5 downTo 1) {
+        print("Step: ")
+        println(item)
+    }
+}
