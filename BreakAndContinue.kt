4d3478abb5cc940d4cc649de1d556aa29ba8fe01
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-02-28 21:23:28 +0530

    21st commit

---
+++ BreakAndContinue.kt
@@ -0,0 +1,13 @@
+fun main() {
+   var x = 0
+   while(x < 10){
+       x++
+        if(x == 5) break
+        println(x)
+    }
+    while(x < 10){
+        x++
+        if(x == 5) continue
+        println(x)
+    }
+}
